dc87567254ad860ecfb188f2562ae0c0b1b5cdc5
yuandadu <duyuanda@yeah.net>
2018-07-08 02:15:47 +0800

    add map row

--- src/test/java/org/jpmml/sparkml/xgboost/XGBoostAuditTest.scala
+++ src/test/java/org/jpmml/sparkml/xgboost/XGBoostAuditTest.scala
@@ -6,7 +6,7 @@ import ml.dmlc.xgboost4j.scala.spark.XGBoostEstimator
 import org.apache.spark.ml.Pipeline
 import org.apache.spark.ml.feature.RFormula
 import org.apache.spark.ml.linalg.DenseVector
-import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.{Row, SparkSession}
 import org.apache.spark.sql.functions.{lit, udf}
 import org.apache.spark.sql.types.{IntegerType, StringType}
 import org.jpmml.sparkml.PMMLBuilder
@@ -17,19 +17,27 @@ object XGBoostAuditTest {
       .master("local")
       .appName("Word Count")
       .getOrCreate()
-    var df = sparkSession.sqlContext.read.option("header", "true").option("inferSchema", "true").csv("csv/Audit.csv")
+    import sparkSession.implicits._
+    var df = sparkSession.read.option("header", "true").option("inferSchema", "true").csv("csv/Audit.csv")
     df = df.withColumn("AdjustedTmp", df("Adjusted").cast(StringType)).drop("Adjusted").withColumnRenamed("AdjustedTmp", "Adjusted")
     var index = 0
-    df.select("age", "Education").columns.map(col => {
-      df.groupBy(col).count().show()
+    var map_count = Map[String, BigInt]()
+    df.select("age", "Hours").columns.map(col => {
+      val count = df.groupBy(col).count().as[(Int, BigInt)]
+      count.collect.map{
+        row => {
+          map_count += index + "_" + row._1 -> row._2
+        }
+      }
       index += 1
     })
+    map_count.foreach(print)
     println(index)
     //df.groupBy("age").count().show()
     val formula = new RFormula().setFormula("Adjusted ~ Age + Income + Gender + Deductions + Hours")
 
     var estimator = new XGBoostEstimator(Map("objective" -> "binary:logistic"))
-    estimator = estimator.set(estimator.round, 101)
+    estimator = estimator.set(estimator.round, 10)
 
     val pipeline = new Pipeline().setStages(Array(formula, estimator))
     val pipelineModel = pipeline.fit(df)
